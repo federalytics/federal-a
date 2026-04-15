5e9dde9e83b7320e50172e71089ccf964a2274e9
federalytics <federalytics@gmail.com>
2026-04-15 14:07:23 -0300

    Update generar_dashboard.py

--- generar_dashboard.py
+++ generar_dashboard.py
@@ -2009,7 +2009,7 @@ if __name__ == '__main__':
     path_carga = sys.argv[1] if len(sys.argv) > 1 else f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet=Carga'
     path_goles = sys.argv[2] if len(sys.argv) > 2 else f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet=Goles'
     template   = Path(sys.argv[3]) if len(sys.argv) > 3 else Path('tfa2026_mini_template.html')
-    output     = Path(sys.argv[4]) if len(sys.argv) > 4 else Path('dashboard.html')
+    output     = Path(sys.argv[4]) if len(sys.argv) > 4 else Path('index.html')
 
     print(f"Leyendo {path_carga}...")
     df_carga = leer_carga(path_carga)
